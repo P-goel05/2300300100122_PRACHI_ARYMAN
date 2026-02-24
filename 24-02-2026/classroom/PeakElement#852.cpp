5c0a316c3c301a5e8c63e4b5bb00355cc7a677a4
P-goel05 <prachigoel831@gmail.com>
2026-02-24 12:10:24 +0530

    PeakElementInMountainArray

---
+++ 24-02-2026/classroom/PeakElement#852.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int s,e,m;
+        s=0;
+        e=arr.size()-1;
+        while(s<e){ //no s eqaul to e condition are the mid is varied
+            m=int(s+(e-s)/2);
+            if(arr[m]<arr[m+1]){ 
+                s=m+1;
+            }
+            else{
+                e=m;//may be we are at peak element (e=mid-1) we lose elements
+            }
+        }
+        return e;
+    }
+};
