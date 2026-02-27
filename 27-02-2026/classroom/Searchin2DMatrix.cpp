ee2e7e375424e99a3fc25d866ef9f4130332707e
P-goel05 <prachigoel831@gmail.com>
2026-02-27 12:32:44 +0530

    threemethodsofsearchingin2dmatrix

---
+++ 27-02-2026/classroom/Searchin2DMatrix.cpp
@@ -0,0 +1,66 @@
+ #include <iostream>
+ #include <vector>
+ using namespace std;
+
+bool BinarySearch(vector<int> arr, int target){
+    int s, e,m;
+    s=0;
+    e=arr.size()-1;
+    while(s<=e){
+        m=int(s+(e-s)/2);
+        if(arr[m]==target){
+            return true;
+        }
+        else if(arr[m]<target){
+            s=m+1;
+        }
+        else{
+            e=m-1;
+        }
+    }
+
+ }
+ int main(){
+    int r,c,i=0,j=0,t;
+    
+    cout<<"enter number of rows of matrix:"<<endl;
+    cin>>r;
+    cout<<"enter number of columns of matrix:"<<endl;
+    cin>>c;
+    cout<<"enter the element to be found:"<<endl;
+    cin>>t;
+    //taking matrix 
+    vector<vector<int>> mat(r,vector<int>(c));
+    //reading the matrix
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            cout<<"enter element:"<<endl;
+            cin>>mat[i][j];
+        }
+    }
+    //  //brute force
+    // for(i=0;i<r;i++){
+    //     for(j=0;j<c;j++){
+    //         if (mat[i][j]==t){
+    //             return true;
+    //         }
+    //         else{
+    //             return false;
+    //         }
+    //     }
+    // }
+    //better
+    for(i=0;i<r;i++){
+        if(mat[i][0]<=t && t<=mat[i][c-1]){
+           if(BinarySearch(mat[i],t)){
+            cout<<"element found"<<endl;
+           }
+           else{
+            cout<<"element not found"<<endl;
+           }
+        }
+    }
+    //optimal 
+    
+    return 0;
+ }
